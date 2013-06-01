590bef52e4908113f2135e36430db24f6b57536f
Phat Wangrungarun <phatograph@gmail.com>
2013-06-01 16:24:46 +0700

    Styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
     raining = CGI::escape(I18n.translate('raining', :locale => :th))
-    tweets = twitter.search_tweets(raining)
-    @tweets = tweets['statuses'].map { |t| t['text'] }
+    # tweets = twitter.search_tweets(raining)
+    # @tweets = tweets['statuses'].map { |t| t['text'] }
+    @tweets = []
   end
 end
